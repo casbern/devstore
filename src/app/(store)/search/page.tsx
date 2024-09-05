9ceebc0cac7eea1d084b10e261a9b6acc2f27eb5
Cássia <cascrisbern@gmail.com>
2024-09-05 15:32:02 +0300

    fix on the grid for the search products page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -43,7 +43,7 @@ export default async function Search({ searchParams }: SearchProps) {
             <Link
               key={product.id}
               href={`/product/${product.slug}`}
-              className=" group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+              className=" group relative   rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
             >
               <Image
                 src={product.image}
