47c8c6e414f35ebf81fb46c80928e2a73664e4db
Cássia <cascrisbern@gmail.com>
2024-09-05 12:23:09 +0300

    created cart component and button components as client componens

---
+++ src/components/add-to-cart-button.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useCart } from '@/contexts/cart-context'
+
+export interface AddToCartButtonProps {
+  productId: number
+}
+
+export function AddToCartButton({ productId }: AddToCartButtonProps) {
+  const { addToCart } = useCart()
+
+  function handleAddProductToCart() {
+    addToCart(productId)
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleAddProductToCart}
+      className="mt-8 flex h-12 items-center font-semibold text-white justify-center rounded-full bg-emerald-600 "
+    >
+      Adicionar ao carrinho
+    </button>
+  )
+}
